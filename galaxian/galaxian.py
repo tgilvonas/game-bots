d915c5a3c59c2c65852ce2584629208dfba05d71
tgilvonas <tgilvonas@gmail.com>
2021-08-21 03:33:39 +0300

    Galaxian bot is in progress: debugging why does it decide not to move

--- galaxian/galaxian.py
+++ galaxian/galaxian.py
@@ -1,8 +1,5 @@
 from pynput.keyboard import Key, Listener, Controller
-import pyautogui, os, time
-
-# path info of files:
-print(os.path.dirname(__file__))
+import pyautogui, os, time, random
 
 print('Hello, I am Python bot and I will try to play old Galaxian game on NES emulator :)')
 print('Press q to start, set focus on the emulator window, and start the game :)')
@@ -11,19 +8,104 @@ scriptPath = os.path.dirname(__file__)
 
 spritesDir = scriptPath + '/sprites/'
 
-#keys:
-keyAccelerate='z'
-keyAccelerateMore='x'
+pixelsOfInsects = [
+	'insect-0.png',
+	'insect-1.png',
+	'insect-2.png'
+]
+pixelsOfEnemyGunfire = 'enemy-gunfire.png'
+
+spriteOfMySpaceship = 'me-smaller.png';
+
+scannableRegionForEnemiesAndGunfire = (2, 156, 276, 290)
+scannableRegionOfMySpaceship = (15, 248, 270, 295)
+
+keyShoot='z'
 keyLeft='n'
 keyRight='m'
 
+keyboard = Controller()
+
+def decideToGoLeftOrRight(coordinatesOfMySpaceship, coordinatesOfEnemyGunfires, coordinatesOfAttackingBugs):
+	minMaxCoordinatesOfAttackingBugs = getMinMaxCoordinatesOfAttackingBugs(coordinatesOfAttackingBugs)
+	whichSideIAm = detectOnWhichSideIsMySpaceship(coordinatesOfMySpaceship)
+	if minMaxCoordinatesOfAttackingBugs != None and whichSideIAm != None:
+		print('I AM ON: ')
+		print(whichSideIAm)
+		print('ENEMIES: ')
+		print(minMaxCoordinatesOfAttackingBugs)
+		if minMaxCoordinatesOfAttackingBugs[0] > 128 and minMaxCoordinatesOfAttackingBugs[1] > 128 and whichSideIAm == 'left' :
+			print('GOING LEFT')
+			goLeft()
+		if minMaxCoordinatesOfAttackingBugs[0] < 128 and minMaxCoordinatesOfAttackingBugs[1] < 128 and whichSideIAm == 'right' :
+			print('GOING RIGHT')
+			goRight()
+		print('GOING ANYWHERE')
+	return None
+
+def goLeft():
+	keyboard.release(keyRight)
+	keyboard.press(keyLeft)
+
+def goRight():
+	keyboard.release(keyLeft)
+	keyboard.press(keyRight)
+
+# not accurate:
+def detectOnWhichSideIsMySpaceship(coordinatesOfMySpaceship):
+	arrayOfSides = ['left', 'right']
+	if coordinatesOfMySpaceship == None:
+		return None
+	if coordinatesOfMySpaceship[0] < 127:
+		return 'left'
+	if coordinatesOfMySpaceship[0] + coordinatesOfMySpaceship[2] > 128:
+		return 'right'
+	return arrayOfSides[random.randint(0,1)]
+
+def getMinMaxCoordinatesOfAttackingBugs(coordinatesOfAttackingBugs):
+	if coordinatesOfAttackingBugs == None:
+		return None
+	arrayOfMinX = [301, 300]
+	arrayOfMaxX = [0, 1]
+	for coordinatesList in coordinatesOfAttackingBugs:
+		arrayOfMinX.append(coordinatesList[0])
+		arrayOfMaxX.append(coordinatesList[0] + coordinatesList[2])
+	return [min(arrayOfMinX), max(arrayOfMaxX)]
+
+# to do:
+def detectIfEnemyGunfireIsGoingToHitMySpaceship(coordinatesOfMySpaceship, coordinatesOfEnemyGunfires):
+	return ''
+
+# to do: move this repeated function to library:
+def detectAGroupOfObjects(arrayOfSprites, scannableScreenRegion):
+	groupOfObjects = []
+	for sprite in arrayOfSprites:
+		groupOfObjects = groupOfObjects + list(pyautogui.locateAllOnScreen(spritesDir + sprite, region=scannableScreenRegion))
+	return groupOfObjects
+
 def playGame():
+
 	print('initialized....')
-	keyboard = Controller()
+
+	coordinatesOfMySpaceship = pyautogui.locateOnScreen(spritesDir + spriteOfMySpaceship, region=scannableRegionOfMySpaceship)
+
 	while 1==1 :
 		try:
-			# next line should be deleted, and the code of game bot should be written:
-			print('It works')
+
+			keyboard.press(keyShoot)
+
+			coordinatesOfMySpaceship = pyautogui.locateOnScreen(spritesDir + spriteOfMySpaceship, region=scannableRegionOfMySpaceship)
+			print('Coordinates of my spaceship:')
+			print(coordinatesOfMySpaceship)
+
+			coordinatesOfEnemyGunfires = list(pyautogui.locateAllOnScreen(spritesDir + pixelsOfEnemyGunfire, region=scannableRegionForEnemiesAndGunfire))
+
+			coordinatesOfAttackingBugs = detectAGroupOfObjects(pixelsOfInsects, scannableRegionForEnemiesAndGunfire)
+
+			keyboard.release(keyShoot)
+
+			decideToGoLeftOrRight(coordinatesOfMySpaceship, coordinatesOfEnemyGunfires, coordinatesOfAttackingBugs)
+
 		except Exception as e:
 			print(e)
 

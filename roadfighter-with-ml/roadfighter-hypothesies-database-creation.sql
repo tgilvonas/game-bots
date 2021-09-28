149e9df0dd17d55396c84d49d7a8ef16298fd9cc
tgilvonas <tgilvonas@gmail.com>
2021-09-28 08:42:05 +0300

    Database creation script was concluded with "passive objects" fields

--- roadfighter-with-ml/roadfighter-hypothesies-database-creation.sql
+++ roadfighter-with-ml/roadfighter-hypothesies-database-creation.sql
@@ -6,12 +6,14 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS hypothesies;
 
 CREATE TABLE hypothesies (
-    id                 INTEGER       PRIMARY KEY AUTOINCREMENT,
-    parent_id                        DEFAULT (0),
-    keys_history       BLOB,
-    hypothesis         BLOB,
-    evaluations        TEXT,
-    average_evaluation DOUBLE (8, 2) 
+    id                        INTEGER       PRIMARY KEY AUTOINCREMENT,
+    parent_id                               DEFAULT (0),
+    keys_history              BLOB,
+    hypothesis                BLOB,
+    evaluations               TEXT,
+    average_evaluation        DOUBLE (8, 2),
+    passive_objects           BLOB,
+    passive_objects_processed BLOB
 );
 
 COMMIT TRANSACTION;

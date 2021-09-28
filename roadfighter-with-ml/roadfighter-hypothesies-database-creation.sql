b549476469e6c5e4f14de15567d8a30acd561e7e
tgilvonas <tgilvonas@gmail.com>
2021-09-28 08:10:55 +0300

    Database creation script was added

---
+++ roadfighter-with-ml/roadfighter-hypothesies-database-creation.sql
@@ -0,0 +1,18 @@
+
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: hypothesies
+DROP TABLE IF EXISTS hypothesies;
+
+CREATE TABLE hypothesies (
+    id                 INTEGER       PRIMARY KEY AUTOINCREMENT,
+    parent_id                        DEFAULT (0),
+    keys_history       BLOB,
+    hypothesis         BLOB,
+    evaluations        TEXT,
+    average_evaluation DOUBLE (8, 2) 
+);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;

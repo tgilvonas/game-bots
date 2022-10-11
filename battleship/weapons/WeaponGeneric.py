6b2a7b4913bccdefebeb20bc2a843d6c92db278f
tomas_g <tomas@artme.lt>
2022-10-11 21:25:47 +0300

    Initiating generic class of weapons

--- battleship/weapons/WeaponGeneric.py
+++ battleship/weapons/WeaponGeneric.py
@@ -2,8 +2,19 @@ from pynput.keyboard import Key, Listener, Controller
 import pyautogui, os, time, random
 
 class WeaponGeneric:
+	
+	scannableScreenRegion = (0, 40, 300, 280)
+
+	keyLeft='n'
+	keyRight='m'
+	keyDown='j'
+	keyUp='h'
+	keySelect='b'
+
+	keyboard = Controller()
+
 	def __init__(self):
 		print('Weapon initiated')
 
 	def shoot(self):
-		print('Boom')
+		print('Boom!')
